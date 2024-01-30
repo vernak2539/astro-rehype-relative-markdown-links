c138166920c14bd9028d27b6fbdb2abac70e0c24
Alex Vernacchia <alex.vernacchia@deliveroo.co.uk>
2024-01-30 17:31:17 +0000

    cleanup: fix naming

--- src/index.mjs
+++ src/index.mjs
@@ -17,7 +17,7 @@ const debug = debugFn("astro-rehype-relative-markdown-links");
 const defaultContentPath = ["src", "content"].join(path.sep);
 
 /** @param options {import('./index').Options} */
-function rehypeAstroRelativeMarkdownLinks(options = {}) {
+function astroRehypeRelativeMarkdownLinks(options = {}) {
   return (tree, file) => {
     visit(tree, "element", (node) => {
       const nodeHref = node.properties.href;
@@ -99,4 +99,4 @@ function rehypeAstroRelativeMarkdownLinks(options = {}) {
   };
 }
 
-export default rehypeAstroRelativeMarkdownLinks;
+export default astroRehypeRelativeMarkdownLinks;
