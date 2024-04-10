906f7b1475ef81d144827729e7627f7c3864d8c6
techfg <barry@techfg.com>
2024-04-10 16:45:56 -0700

    chore: add comments to explain methodology for transforming url

--- src/index.mjs
+++ src/index.mjs
@@ -55,14 +55,48 @@ function astroRehypeRelativeMarkdownLinks(options = {}) {
       const frontmatterSlug = frontmatter.slug;
       const contentDir = options.contentPath || defaultContentPath;
 
+      /*
+        By default, Astro assumes content collections are subdirectories of a content path which by default is src/content.
+        It then treats all content in a content collection as relative to the collection itself, not the site.  For example,
+        a page at /src/content/docs/foo/bar/my-page.md would have a slug of foo/bar/my-page rather than docs/foo/bar/my-page 
+        as foo/bar/my-page is the path relative to the content collection.  This allows Astro to map content collections to 
+        multiple page paths (e.g., /pages/blog/[...slug].astro) & /pages/[year]/[month]/[day]/[slug].astro) to enable reusing 
+        content collections. 
+        
+        Given this, we need to extract the collection name and include it in the generated path.  Since we don't have internal 
+        info, we have to make an assumption that the site page path is mapped directly to a path that starts with the collection
+        name followed by the slug of the content collection page.  We do this by following Astro's own assumptions on the directory 
+        structure of content collections - they are subdirectories of content path.
+
+        KNOWN LIMITATIONS/ISSUES
+        - Astro allows pages within a content collection to be excluded (see https://docs.astro.build/en/guides/routing/#excluding-pages).  
+        We currently do not adhere to this logic (See https://docs.astro.build/en/guides/routing/#excluding-pages).
+        - Astro allows mapping a content collection to multiple site paths (as mentioned above).  The current approach of this library
+        assumes that page paths always align 1:1 to their corresponding content collection paths based on physical directory name.  For 
+        example, if you have a content collection at src/content/blogs but your site page path is at /pages/my-blog/[...slug].astro,
+        the default functionality of this library will not work currently.  See https://github.com/vernak2539/astro-rehype-relative-markdown-links/issues/24.
+      */
+
+      // determine the path of the target file relative to the content path
       const relativeToContentPath = path.relative(contentDir, relativeFile);
+      // determine the collection name using Astros default assumption that content collections are subdirs of content path
       const collectionName = path.dirname(relativeToContentPath).split(path.posix.sep)[0];
+      // determine the path of the target file relative to the collection
+      // since the slug for content collection pages is always relative to collection root
       const relativeToCollectionPath = path.relative(collectionName, relativeToContentPath);
+      // md/mdx extentions should not be in the final url
       const withoutFileExt = replaceExt(relativeToCollectionPath, "")
+      // each path segment should be individulally sluggified
       const pathSegments = withoutFileExt.split(path.posix.sep);
+      // Astro generates a slug for each page on the site as a fallback if the page does not have a custom slug
       const generatedSlug = generateSlug(pathSegments);
+      // if we have a custom slug, use it, else use the default
       const resolvedSlug = resolveSlug(generatedSlug, frontmatterSlug);
 
+      // content collection slugs are relative to content collection root
+      // so build url including the content collection name and the pages slug
+      // NOTE - This only handles situations where the name of the content collection is directly
+      //        mapped to the site page serving the content collection page (see details above)
       let webPathFinal = path.posix.sep +
         [
           collectionName,
